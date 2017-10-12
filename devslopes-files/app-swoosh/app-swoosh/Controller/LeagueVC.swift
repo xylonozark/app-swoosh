6283a9cf80f0ed7998f8e31a2d8000f0210ae14f
Xantes Fabela <xantesfabela@172-242-4-124.cust.exede.net>
2017-10-11 19:25:05 -0500

    ib actions and modeling

--- devslopes-files/app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ devslopes-files/app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,19 +10,38 @@ import UIKit
 
 class LeagueVC: UIViewController {
   
+    var player: Player!
+    
     @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
       performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
     }
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+       selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ devslopes-files/app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Xantes Fabela on 10/11/17.
+//  Copyright © 2017 Xantes Fabela. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
